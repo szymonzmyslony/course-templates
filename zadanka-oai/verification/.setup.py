92f434082480a804b40445e77550f603e45aad04
tymowskyy <tymowskyy@gmail.com>
2025-12-07 22:04:50 +0100

    (zadanka-oai): add train to verification in restrooms

--- zadanka-oai/verification/.setup.py
+++ zadanka-oai/verification/.setup.py
@@ -1,5 +1,7 @@
 
 import zipfile
+with zipfile.ZipFile('train.zip', 'r') as zip_ref:
+    zip_ref.extractall()
 with zipfile.ZipFile('test_set.zip', 'r') as zip_ref:
     zip_ref.extractall()
 with zipfile.ZipFile('validation_set.zip', 'r') as zip_ref:
