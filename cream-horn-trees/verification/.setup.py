0d76d457021ea0c937c0c9cee1def831125be6a2
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2026-01-21 22:18:23 +0100

    Load dataframes in verification setup

--- cream-horn-trees/verification/.setup.py
+++ cream-horn-trees/verification/.setup.py
@@ -2,3 +2,15 @@
 import zipfile
 with zipfile.ZipFile('images.zip', 'r') as zip_ref:
     zip_ref.extractall()
+
+# Load data into global namespace for student code
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+labels_train_df = pd.read_csv('labels_train.csv')
+labels_valid_df = pd.read_csv('labels_valid.csv')
+labels_test_df = pd.read_csv('labels_test.csv')
+trees_valid_df = pd.read_csv('trees_valid.csv')
+trees_test_df = pd.read_csv('trees_test.csv')
