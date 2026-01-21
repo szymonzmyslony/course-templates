571c84446e23ac055417313bb19bb99453a93a9b
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2026-01-21 22:21:44 +0100

    Add tqdm import

--- cream-horn-trees/verification/.setup.py
+++ cream-horn-trees/verification/.setup.py
@@ -8,6 +8,7 @@ import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 labels_train_df = pd.read_csv('labels_train.csv')
 labels_valid_df = pd.read_csv('labels_valid.csv')
