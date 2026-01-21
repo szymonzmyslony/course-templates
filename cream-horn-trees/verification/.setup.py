5f305062a551e60f9135816c3c4b71eafa261cdc
Szymon Zmyslony <szymon.zmyslony@gmail.com>
2026-01-21 22:27:32 +0100

    Add TYPE_INDEX_MAP and file paths from scaffold

--- cream-horn-trees/verification/.setup.py
+++ cream-horn-trees/verification/.setup.py
@@ -10,10 +10,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
+# Type mapping from scaffold cells
+TYPE_INDEX_MAP = {
+    'czekolada_sondey': 0,
+    'czekolada_banitki': 1,
+    'wanilia_sondey': 2,
+    'orzechy_banitki': 3
+}
+
+# File paths
+LABELS_TRAIN_FILE = 'labels_train.csv'
+LABELS_VALIDATION_FILE = 'labels_valid.csv'
+TREES_VALIDATION_FILE = 'trees_valid.csv'
+
 labels_train_df = pd.read_csv('labels_train.csv')
 labels_valid_df = pd.read_csv('labels_valid.csv')
 labels_validation_df = labels_valid_df  # alias
 labels_test_df = pd.read_csv('labels_test.csv')
 trees_valid_df = pd.read_csv('trees_valid.csv')
 trees_validation_df = trees_valid_df  # alias
+trees_validation_df['tree'] = trees_validation_df['tree'].map(lambda x: eval(x))
 trees_test_df = pd.read_csv('trees_test.csv')
